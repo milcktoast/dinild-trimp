954c6f7a998aaad496637bff2db320f71620026b
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-10-15 17:56:24 -0700

    Add build notification

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,4 @@ export NODE_ENV=production
 node ./scripts/preprocess.js src/index.js > src/index.prod.js
 browserify -t [ rollupify --config rollup.config.js ] src/index.prod.js | uglifyjs -cn > build/index.js
 rm src/index.prod.js
+terminal-notifier -title "Trimp" -message "Build Complete"
