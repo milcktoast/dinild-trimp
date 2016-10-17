09fc59746886c4cd1db0e3c9aa93d674a75885e0
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-10-17 12:35:21 -0700

    Add model load time logging

--- src/utils/model-load.js
+++ src/utils/model-load.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch'
+import { logger } from './logger'
 
 const FLOAT_ATTR_KEYS = [
   'normals',
@@ -15,10 +16,15 @@ const JSON_ATTR_KEYS = [
   'boneFrames'
 ]
 
+const LOG_KEYS = {
+  loadModel: 'load model'
+}
+
 const fetchFloatBuffers = createBufferFetcher(Float32Array)
 const fetchIntBuffers = createBufferFetcher(Uint16Array)
 
 export function loadModel (baseUrl, modelJson) {
+  logger.time(LOG_KEYS.loadModel)
   const mapBufferUrl = (key) => ({
     key,
     url: formatDestPath(baseUrl, key, 'bin')
@@ -46,28 +52,41 @@ export function loadModel (baseUrl, modelJson) {
   ]).then((resolutions) => resolutions.reduce((props, data) => {
     props[data.key] = data.payload
     return props
-  }, {}))
+  }, {})).then((props) => {
+    logger.timeEnd(LOG_KEYS.loadModel)
+    return props
+  })
 }
 
 function createBufferFetcher (BufferCtor) {
   return ({key, url}) => new Promise((resolve, reject) => {
+    const logKey = `| fetch ${key}`
+    logger.time(logKey)
     fetch(url)
       .then((res) => res.arrayBuffer())
       .then((buffer) => resolve({
         key,
         payload: new BufferCtor(buffer)
-      }))
+      })).then((resolution) => {
+        logger.timeEnd(logKey)
+        return resolution
+      })
   })
 }
 
 function fetchJson ({key, url}) {
+  const logKey = `| fetch ${key}`
   return new Promise((resolve, reject) => {
+    logger.time(logKey)
     fetch(url)
       .then((res) => res.json())
       .then((json) => resolve({
         key,
         payload: json
-      }))
+      })).then((resolution) => {
+        logger.timeEnd(logKey)
+        return resolution
+      })
   })
 }
 
