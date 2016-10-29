f0de68095df79005c7dbc4eb3f2b612ad9ac20f1
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-10-28 19:33:06 -0700

    Update word configurations

--- src/constants/animation.js
+++ src/constants/animation.js
@@ -1,14 +1,15 @@
 // TODO: Create generic consonant frame
 export const MOUTH_FRAMES = [
   '_', // Create blank frame
-  'A,I',
-  'L,N,D', // Probably add unique frames for N, D
-  'E',
-  'O',
-  'OO,U',
-  'W,Q',
-  'M,B,P',
-  'F,V'
+  'A-I',
+  'L-N-D-T', // Probably add unique frames for N, D, T
+  'EE',
+  'S',
+  'O-R', // Add unique frome for R
+  'OO-U',
+  'W-Q',
+  'M-B-P',
+  'F-V'
 ]
 
 export const MOUTH_FRAMES_MAP = MOUTH_FRAMES
@@ -22,7 +23,7 @@ export const MOUTH_FRAMES_SHAPE_MAP = expandFrameKeys(MOUTH_FRAMES)
 function expandFrameKeys (frames) {
   const expanded = {}
   frames.forEach((key, index) => {
-    key.split(',').forEach((char) => {
+    key.split('-').forEach((char) => {
       expanded[char] = index
     })
   })
--- src/constants/phrase.js
+++ src/constants/phrase.js
@@ -2,24 +2,92 @@ export const WORDS = [
   {
     name: 'childhood',
     syllables: [{
-      duration: 24,
+      duration: 30,
       shape: 'CH-I-L',
       weight: 1
     }, {
-      duration: 18,
+      duration: 24,
       shape: 'H-OO-D',
       weight: 0.7
     }]
+  },
+
+  {
+    name: 'deal',
+    syllables: [{
+      duration: 24,
+      shape: 'D-EE-L',
+      weight: 1
+    }]
+  },
+
+  {
+    name: 'honestly',
+    syllables: [{
+      duration: 8,
+      shape: 'A-A',
+      weight: 0.7
+    }, {
+      duration: 12,
+      shape: 'N-S',
+      weight: 0.7
+    }, {
+      duration: 18,
+      shape: 'T-EE',
+      weight: 1
+    }]
+  },
+
+  {
+    name: 'love',
+    syllables: [{
+      duration: 18,
+      shape: 'L-O',
+      weight: 1
+    }, {
+      duration: 18,
+      shape: 'V-A',
+      weight: 0.7
+    }]
+  },
+
+  {
+    name: 'people',
+    syllables: [{
+      duration: 18,
+      shape: 'P-EE',
+      weight: 1
+    }, {
+      duration: 18,
+      shape: 'P-L',
+      weight: 0.8
+    }]
+  },
+
+  {
+    name: 'pussy',
+    syllables: [{
+      duration: 18,
+      shape: 'P-O',
+      weight: 0.8
+    }, {
+      duration: 18,
+      shape: 'S-EE',
+      weight: 1
+    }]
+  },
+
+  {
+    name: 'strict',
+    syllables: [{
+      duration: 30,
+      shape: 'S-T-R-I-C-T'
+    }]
   }
-  // 'deal'
-  // 'honestly',
-  // 'love',
-  // 'people',
-  // 'pussy',
-  // 'strict'
 ]
 
 export const WORD_LOCATIONS = [
-  0.575829, 0.433556
-  // 0.557586, 0.280022
+  0.575829, 0.433556,
+  0.557586, 0.280022,
+  0.461130, 0.290126
 ]
