2495830cdb350d9a880c8f608c4d9d88f4f812b5
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-11-04 11:01:44 -0700

    Update logger to ensure console

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,16 +5,28 @@ const METHODS = [
   'timeEnd'
 ]
 
+function noop () {}
+
+function ensureConsole () {
+  if (window.console) return window.console
+  const noopConsole = {}
+  METHODS.forEach((name) => {
+    noopConsole[name] = noop
+  })
+  return noopConsole
+}
+
 function createLogger () {
+  const _console = ensureConsole()
   const logger = {}
   METHODS.forEach((name) => {
-    logger[name] = (...args) => console[name](...args)
+    logger[name] = (...args) => _console[name](...args)
   })
   logger.logHash = (name, hash) => {
     Object.keys(hash).forEach((key) => {
-      console.log(`|  ${key}: ${hash[key]}`)
+      _console.log(`|  ${key}: ${hash[key]}`)
     })
-    console.log(name)
+    _console.log(name)
   }
   return logger
 }
